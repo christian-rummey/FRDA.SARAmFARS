ba6bb24de893370d0296668f7e9dbf964ac66cdc
christian-rummey <christian.rummey@gmail.com>
2022-03-10 11:18:27 +0100

    .

--- 0.DM.R
+++ 0.DM.R
@@ -23,40 +23,16 @@ knitr::opts_chunk$set(warning  = F)
 # packages ----------------------------------------------------------------
 rm(list = ls())
 
-# data --------------------------------------------------------------------
-
-pars       <- c(
-  'SARA',  'SARA.ax', 'SARA.ki',
-  'mFARS', 'FARS.E' , 'FARS.B',
-  'ICARS', 'ICARS.gp', 'ICARS.ki'
-)
-
-
-dt. <- bind_rows(
-  .dd.FA('fars')  %>% filter(study == 'FACOMS', paramcd %in% pars),
-  .dd.FA('icars') %>% filter(study == 'FACOMS', paramcd %in% pars),
-  .dd.FA('sara')  %>% filter(study == 'FACOMS', paramcd %in% pars)
-  ) %>% 
-  select(-fpf, -hpf) %>% 
-  spread( paramcd, aval ) %>% 
-  filter( !is.na(SARA) | !is.na(ICARS)) %>% 
-  gather( paramcd, aval, pars) %>% 
-  .add.time() %>% 
-  .add.demo(l = 3 ) %>% 
-  left_join( .dd.FA('steps') %>% select(sjid, avisitn, amb)  )
-
-dt. %>% 
-  .wds('DATA derived/long.data')
-
 # data for progression ----------------------------------------------------
 
 dt. <- bind_rows(
-  .dd.FA('icars.slope.chg') %>% filter(paramcd == 'ICARS'),
-  .dd.FA('sara.slope.chg') %>% filter(paramcd == 'SARA'),
-  .dd.FA('fars.slope.chg') %>% filter(paramcd == 'mFARS')
+  .dd.FA('icars.slope.chg') %>% filter(paramcd %in% c('ICARS','ICARS.ax','ICARS.ki')),
+  .dd.FA('sara.slope.chg')  %>% filter(paramcd %in% c('SARA','SARA.ax','SARA.ki','SARA.ku')),
+  .dd.FA('fars.slope.chg')  %>% filter(paramcd %in% c('FARSn','mFARS','FARS.E','FARS.BC','FARS.B'))
 ) %>% 
   droplevels() %>% 
-  select(-fpf, -hpf)
+  select(-c('fpf', 'hpf', 'bl', 'bl.age')) %>% 
+  filter(study == 'FACOMS')
 
 dt. <- .dd.FA('icars') %>% filter(paramcd == 'ICARS') %>% select(study, sjid, avisitn) %>% 
   left_join(dt.)
@@ -64,81 +40,53 @@ dt. <- .dd.FA('icars') %>% filter(paramcd == 'ICARS') %>% select(study, sjid, av
 with(dt., table(int, forslope, exclude = F))
 
 dt. %>% 
-  filter(sjid == 4201, paramcd == 'mFARS') %>% 
+  filter(sjid == 4383) %>% 
+  # filter(sjid == 4201, paramcd == 'mFARS') %>% 
   # filter(!is.na(int))
   mutate()
 
-levels( dt.$paramcd )
-levels( dt.$age.grp )
-range ( dt.$age )
+dt. %<>% 
+    filter(!is.na(age))
 
 dt. %<>% 
   mutate( age.grp = cut(age, c(0,8,12,16,25,40,100), labels = c('<8y', '8-11y', '12-15y', '16-24y',           '25-40y', '>40y' ), right = T))
+
+
+# adjust differential staring times to this subset ----------------------------------------
+
+dt. %<>%
+  group_by(sjid, paramcd, amb) %>%
+  mutate(time. = time. - min(time.))
+
+
+# initial version ---------------------------------------------------------
+
+# pars       <- c(
+#   'SARA',  'SARA.ax', 'SARA.ki',
+#   'mFARS', 'FARS.E' , 'FARS.B',
+#   'ICARS', 'ICARS.gp', 'ICARS.ki'
+# )
+# 
+# dt. <- bind_rows(
+#   .dd.FA('fars')  %>% filter(study == 'FACOMS', paramcd %in% pars),
+#   .dd.FA('icars') %>% filter(study == 'FACOMS', paramcd %in% pars),
+#   .dd.FA('sara')  %>% filter(study == 'FACOMS', paramcd %in% pars)
+#   ) %>% 
+#   select(-fpf, -hpf) %>% 
+#   spread( paramcd, aval ) %>% 
+#   filter( !is.na(SARA) | !is.na(ICARS)) %>% 
+#   gather( paramcd, aval, pars) %>% 
+#   .add.time() %>% 
+#   .add.demo(l = 3 ) %>% 
+#   left_join( .dd.FA('steps') %>% select(sjid, avisitn, amb)  )
+# 
+# write -------------------------------------------------------------------
+
+dt. %>%
+  .wds('DATA derived/long.data')
+
   
 
-# demo. <- dt. %>% 
-#   group_by( sjid ) %>% 
-#   filter  ( avisitn == min(avisitn) ) %>% 
-#   select  ( study, sjid, avisitn, paramcd, aval) %>% 
-#   spread  ( paramcd, aval)
-
-
-# dt.  <- read_excel('../DATA/Melbourne/Copy of FACOMS SARA ICARS.xlsx') %>% 
-#   mutate( adt = as.Date(`Assessment date`)) %>% 
-#   rename( sjid = `CCRN Subject ID`) %>% 
-#   select(-`Assessment date`) %>% 
-#   mutate_at('sjid', as.character) %>% 
-#   select(sjid, adt, everything())
-# 
-# fars. <- .dd.FA('FARS') %>% 
-#   filter( paramcd %in% c('mFARS','FARSn','FARS.E')) %>% 
-#   spread( paramcd, aval ) %>% 
-#   select( sjid, adt, avisitn, mFARS, FARSn, FARS.E )
-# 
-# dt. %<>% 
-#   select(sjid, adt, SARA_Total, ICARS_Total) %>% 
-#   left_join(fars.)
-# 
-# dt.missing <- dt. %>% 
-#   filter(is.na(avisitn))
-# 
-# dt.missing %>% 
-#   ggplot()+geom_histogram()+
-#   aes(x = adt)+
-#   scale_y_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10))
-# 
-# dt.missing %<>% 
-#   mutate(yov = lubridate::year(adt)) %>% 
-#   rename(adt.melbourne = adt ) %>% 
-#   select(-avisitn)
-# 
-# .ds.FACOMS('faneuroe') %>% 
-#   select(sjid, avisitn, adt) %>% 
-#   mutate(yov = lubridate::year(adt)) %>% 
-#   rename(adt.facoms = adt ) %>% 
-#   right_join( dt.missing ) %>% 
-#   select(sjid, avisitn, yov, adt.facoms, adt.melbourne) %>% 
-#   mutate(adt.facoms - adt.melbourne) %>% 
-#   arrange(sjid, adt.facoms) %>% 
-#   as.data.frame()
-# 
-# 
-# dt. %<>% 
-#   filter(!is.na(mFARS)) %>% 
-#   rename(SARA = SARA_Total, ICARS = ICARS_Total)
-# 
-# 
-# # . -----------------------------------------------------------------------
-# 
-# dt. %>% 
-#   left_join(.dd.FA('steps') %>% select(sjid, avisitn, amb)) %>% 
-#   ggplot()+geom_jitter()+
-#   aes(x = FARS.E)+
-#   aes(y = SARA)+
-#   aes(color = amb)+
-#   geom_smooth(method = 'lm', se = F)+
-#   ggpubr::stat_cor()+
-#   geom_line(aes(group = sjid))
-# 
-# 
-# 
+table( dt.$paramcd )
+levels( dt.$age.grp )
+range ( dt.$age )
